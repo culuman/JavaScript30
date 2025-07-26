e9862cb919c008b2cbaea60fed8379f0767c8307
Nikola Culuman <nculuman65@gmail.com>
2025-07-26 21:38:22 +0200

    Add CSS + JS Clock project

---
+++ CSS+JS_Clock/readme.md
@@ -0,0 +1,4 @@
+# CSS + JavaScript Clock
+
+A simple analog clock that dynamically updates the hands in real time based on the current system time.  
+I also added a digital clock below the original analog one from the tutorial.
---
+++ CSS+JS_Clock/script.js
@@ -0,0 +1,28 @@
+const secHand = document.querySelector('.sec-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+const digitalTime = document.getElementById('digital-time');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  const secondsFormatted = String(seconds).padStart(2, '0');
+  const minutesFormatted = String(minutes).padStart(2, '0');
+  const hoursFormatted = String(hours).padStart(2, '0');
+  digitalTime.textContent = `${hoursFormatted}:${minutesFormatted}:${secondsFormatted}`;
+}
+
+setInterval(setDate, 1000);
