348ad93bcb95f27e7986c6d3007377d14d68707d
Nikola Culuman <nculuman65@gmail.com>
2025-07-27 12:39:33 +0200

    Improve clock styling: implement hands with CSS pseudo-elements

--- CSS+JS_Clock/script.js
+++ CSS+JS_Clock/script.js
@@ -8,15 +8,15 @@ function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const secondsDegrees = ((seconds / 60) * 360);
   secHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const minutes = now.getMinutes();
-  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  const minutesDegrees = ((minutes / 60) * 360);
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 12) * 360) + 90;
+  const hoursDegrees = ((hours / 12) * 360);
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 
   const secondsFormatted = String(seconds).padStart(2, '0');
